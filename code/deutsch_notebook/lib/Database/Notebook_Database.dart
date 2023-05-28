30e409a15b9e29eb58f7742c536d035356b03213
Sky <Sky.Rivera89@gmail.com>
2023-05-29 02:35:56 +0330

    Notebook Database

---
+++ code/deutsch_notebook/lib/Database/Notebook_Database.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'package:deutsch_notebook/Models/Notebook_Model.dart';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'Notebook_Database.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE NoteBook(Note_Id INTEGER PRIMARY KEY, Note_Name TEXT)',
+      );
+    },
+    version: 1,
+  );
+
+  Future<void> Insert_Notebook(Notebook_Model notebook_model) async {
+    final db = await database;
+
+    await db.insert(
+      'NoteBook',
+      notebook_model.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Notebook_Model>> Notebooks() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('NoteBook');
+
+    return List.generate(maps.length, (i) {
+      return Notebook_Model(
+        Notebook_ID: maps[i]['Notebook_ID'],
+        Notebook_Name: maps[i]['Notebook_Name'],
+      );
+    });
+  }
+
+  Future<void> UpdateNotebook(Notebook_Model notebook_model) async {
+    final db = await database;
+
+    await db.update(
+      'NoteBook',
+      notebook_model.toMap(),
+      where: 'Notebook_ID = ?',
+      whereArgs: [notebook_model.Notebook_ID],
+    );
+  }
+
+  Future<void> DeleteNotebook(int id) async {
+    final db = await database;
+    await db.delete(
+      'NoteBook',
+      where: 'Notebook_ID = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<int?> NotebookLength() async {
+    final db = await database;
+    return Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM NoteBook'));
+  }
+
+}
--- code/deutsch_notebook/lib/Models/Notebook_Model.dart
+++ code/deutsch_notebook/lib/Models/Notebook_Model.dart
@@ -1,7 +1,19 @@
 class Notebook_Model {
 
-  String? Notebook_ID;
-  String? Notebook_Name;
+  final String Notebook_ID;
+  final String Notebook_Name;
 
-  Notebook_Model(this.Notebook_ID, this.Notebook_Name);
+  Notebook_Model({required this.Notebook_ID, required this.Notebook_Name});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'Notebook_ID': Notebook_ID,
+      'Notebook_Name': Notebook_Name,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Notebook{Notebook_ID: $Notebook_ID, Notebook_Name: $Notebook_Name}';
+  }
 }
